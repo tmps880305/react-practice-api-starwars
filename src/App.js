6c6c835858e72fd5bee49971066cf5cd197ccf00
randy <tmps880305@gmail.com>
2022-12-01 22:37:10 -0500

    Handling HTTP Errors

--- src/App.js
+++ src/App.js
@@ -6,20 +6,30 @@ import './App.css';
 function App() {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     async function fetchMovieHandler() {
         setIsLoading(true);
-        const response = await fetch('https://swapi.dev/api/films');
-        const data = await response.json();
-
-        const transformedData = data.results.map(moviedata => {
-            return {
-                id: moviedata.episode_id,
-                title: moviedata.title,
-                openingText: moviedata.opening_crawl
+        setError(null);
+        try {
+            const response = await fetch('https://swapi.dev/api/films');
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
             }
-        });
-        setMovies(transformedData);
+
+            const data = await response.json();
+
+            const transformedData = data.results.map(moviedata => {
+                return {
+                    id: moviedata.episode_id,
+                    title: moviedata.title,
+                    openingText: moviedata.opening_crawl
+                }
+            });
+            setMovies(transformedData);
+        } catch (error) {
+            setError(error.message);
+        }
         setIsLoading(false);
     }
 
@@ -30,7 +40,8 @@ function App() {
             </section>
             <section>
                 {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
-                {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+                {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+                {!isLoading && error && <p>{error}</p>}
                 {isLoading && <p>Movie is loading...</p>}
             </section>
         </React.Fragment>
