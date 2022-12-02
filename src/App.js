e306fd6375679e225f3e51c917dd8a7910a267d2
randy <tmps880305@gmail.com>
2022-12-02 02:32:30 -0500

    Create Custom hook (useHttp) for HTTP requests 1. Change the GET part

--- src/App.js
+++ src/App.js
@@ -2,53 +2,62 @@ import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import AddMovie from './components/AddMovie'
+import useHttp from './hooks/use-http';
 import './App.css';
 
 function App() {
     const [movies, setMovies] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [error, setError] = useState(null);
 
-    const fetchMovieHandler = useCallback(async () => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch('https://react-http-aa7a6-default-rtdb.firebaseio.com/movie.json');
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-            }
-
-            const data = await response.json();
-
-            const loadMovies = [];
-            for (const key in data) {
-                loadMovies.push({
-                    id: key,
-                    title: data[key].title,
-                    openingText: data[key].openingText,
-                    releaseDate: data[key].releaseDate
-                });
-            }
-            setMovies(loadMovies);
-
-            // const transformedData = data.results.map(moviedata => {
-            //     return {
-            //         id: moviedata.episode_id,
-            //         title: moviedata.title,
-            //         openingText: moviedata.opening_crawl
-            //     }
-            // });
-            // setMovies(transformedData);
-
-        } catch (error) {
-            setError(error.message);
+    const requestMovieConfig = {url: 'https://react-http-aa7a6-default-rtdb.firebaseio.com/movie.json'};
+    const transformMovies = moviesObj => {
+        const loadMovies = [];
+        for (const key in moviesObj) {
+            loadMovies.push({
+                id: key,
+                title: moviesObj[key].title,
+                openingText: moviesObj[key].openingText,
+                releaseDate: moviesObj[key].releaseDate
+            });
         }
-        setIsLoading(false);
-    }, []);
+        setMovies(loadMovies);
+    };
+
+    const {isLoading, error, sendRequest: fetchMovieHandler} = useHttp(requestMovieConfig, transformMovies);
+
+    // const fetchMovieHandler = useCallback(async () => {
+    //     setIsLoading(true);
+    //     setError(null);
+    //     try {
+    //         const response = await fetch('https://react-http-aa7a6-default-rtdb.firebaseio.com/movie.json');
+    //         if (!response.ok) {
+    //             throw new Error(`Error: ${response.status}`);
+    //         }
+    //
+    //         const data = await response.json();
+    //
+    //         const loadMovies = [];
+    //         for (const key in data) {
+    //             loadMovies.push({
+    //                 id: key,
+    //                 title: data[key].title,
+    //                 openingText: data[key].openingText,
+    //                 releaseDate: data[key].releaseDate
+    //             });
+    //         }
+    //         setMovies(loadMovies);
+    //
+    //     } catch (error) {
+    //         setError(error.message);
+    //     }
+    //     setIsLoading(false);
+    // }, []);
 
     useEffect(() => {
         fetchMovieHandler();
-    }, [fetchMovieHandler]);
+    }, []);
+
 
     const addMovieHandler = async (movie) => {
         const response = await fetch('https://react-http-aa7a6-default-rtdb.firebaseio.com/movie.json', {
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const useHttp = () => {
+const useHttp = (httpRequestConfig, applyData) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
+    const sendRequest = async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(httpRequestConfig.url, {
+                method: httpRequestConfig.method ? httpRequestConfig.method : 'GET',
+                headers: httpRequestConfig.headers ? httpRequestConfig.headers : {},
+                body: httpRequestConfig.body ? httpRequestConfig.body : null
+            });
 
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
 
+            const data = await response.json();
+            applyData(data);
 
+        } catch (error) {
+            setError(error.message || 'Something went wrong.');
+        }
+        setIsLoading(false);
+    };
+
+    return {
+        isLoading,
+        error,
+        sendRequest
+
+        // Same as:
+        // isLoading: isLoading,
+        // error: error,
+        // sendRequest: sendRequest
+    }
 };
 
 export default useHttp;
